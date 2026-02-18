6b5064173b51a7ab564141b7ad21b3dc0bb79a1d
DuCrato <williamanderson1994@hotmail.com>
2026-02-18 15:20:59 -0300

    feat: Adição de Migrations

---
+++ src/Fleet.Infrastructure/Migrations/20260218062849_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fleet.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Veiculos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Placa = table.Column<string>(type: "nvarchar(7)", maxLength: 7, nullable: false),
+                    Renavam = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    Chassi = table.Column<string>(type: "nvarchar(17)", maxLength: 17, nullable: false),
+                    NomeProprietario = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: false),
+                    Cidade = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Veiculos");
+        }
+    }
+}
---
+++ src/Fleet.Infrastructure/Migrations/20260218180222_AddUniqueIndexes.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fleet.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Veiculos_Chassi",
+                table: "Veiculos",
+                column: "Chassi",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Veiculos_Placa",
+                table: "Veiculos",
+                column: "Placa",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Veiculos_Renavam",
+                table: "Veiculos",
+                column: "Renavam",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Veiculos_Chassi",
+                table: "Veiculos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Veiculos_Placa",
+                table: "Veiculos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Veiculos_Renavam",
+                table: "Veiculos");
+        }
+    }
+}
